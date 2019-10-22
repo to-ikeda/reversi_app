d3d6e18ebe27f3271392a20930a24d2cb0da2221
DESKTOP-LHEO9NV\tomoy <tomoyaikeda@gmail.com>
2019-10-22 12:44:44 +0900

    プレーヤによって白と黒を変える

--- reversi_pjt/reversi/Assets/Scripts/BordScript.cs
+++ reversi_pjt/reversi/Assets/Scripts/BordScript.cs
@@ -9,16 +9,22 @@ public class BordScript : MonoBehaviour, IPointerClickHandler
     public GameObject piece;
     public Material[] materials;
 
+    // ゲームマネージャー
+    GameObject gm;
+    GameManager gmScript;
+
     // Start is called before the first frame update
     void Start()
     {
+        gm = GameObject.Find("GameManager");
+        gmScript = gm.GetComponent<GameManager>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     // ボードクリック時の処理
@@ -27,11 +33,17 @@ public class BordScript : MonoBehaviour, IPointerClickHandler
         //石を表示
         piece.SetActive(true);
 
-        //石を白に変更
-        piece.GetComponent<Renderer>().material = materials[1];
-
-        //石を黒に変更
-        //piece.GetComponent<Renderer>().material = materials[0];
+        if (gmScript.player == "black")
+        {
+            //石を黒に変更
+            piece.GetComponent<Renderer>().material = materials[0];
+            Debug.Log(gmScript.player);
+        }
+        else {
+            //石を白に変更
+            piece.GetComponent<Renderer>().material = materials[1];
+            Debug.Log(gmScript.player);
+        }
 
     }
 
