05e72cb95e6a0e48e58b45f726ec2de0cc9f7b8d
DESKTOP-LHEO9NV\tomoy <tomoyaikeda@gmail.com>
2019-10-22 10:53:04 +0900

    ボードをクリックして石を置くところまで(1セル)

---
+++ reversi_pjt/reversi/Assets/Scripts/BordScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BordScript : MonoBehaviour, IPointerClickHandler
+{
+
+    public GameObject piece;
+    public Material[] materials;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // ボードクリック時の処理
+    public void OnPointerClick(PointerEventData data)
+    {
+        //石を表示
+        piece.SetActive(true);
+
+        //石を白に変更
+        piece.GetComponent<Renderer>().material = materials[1];
+
+        //石を黒に変更
+        //piece.GetComponent<Renderer>().material = materials[0];
+
+    }
+
+}
