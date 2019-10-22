60e99f357fa6e8ba7582c0b480e7b1ba1707b08d
Gomez-KU <>
2019-10-22 16:47:13 +0900

    テスト

---
+++ k_ushigome_work/reversi_pjt/reversi/Assets/Scripts/BordScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BordScript : MonoBehaviour, IPointerClickHandler
+{
+
+    public GameObject piece;
+    public Material[] materials;
+
+    // ゲームマネージャー
+    GameObject gm;
+    GameManager gmScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameObject.Find("GameManager");
+        gmScript = gm.GetComponent<GameManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // ボードクリック時の処理
+    public void OnPointerClick(PointerEventData data)
+    {
+        //石を表示
+        //piece.SetActive(true);
+        Instantiate(piece, transform.position, transform.rotation);
+
+        if (gmScript.player == "black")
+        {
+            //石を黒に変更
+            piece.GetComponent<Renderer>().material = materials[0];
+            Debug.Log(gmScript.player);
+        }
+        else {
+            //石を白に変更
+            piece.GetComponent<Renderer>().material = materials[1];
+            Debug.Log(gmScript.player);
+        }
+
+    }
+
+}
