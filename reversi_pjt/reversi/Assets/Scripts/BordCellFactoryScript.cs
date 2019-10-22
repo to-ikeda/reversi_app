fab4773ea34eedee20cc83b2b3f3f1ee652109b0
Gomez-KU <>
2019-10-22 23:26:15 +0900

    ・スクリプトの更新 reversi_pjt/reversi/Assets/Scripts/BordCellFactoryScript.cs reversi_pjt/reversi/Assets/Scripts/BordScript.cs
    ・k_ushigome_workディレクトリ削除

--- reversi_pjt/reversi/Assets/Scripts/BordCellFactoryScript.cs
+++ reversi_pjt/reversi/Assets/Scripts/BordCellFactoryScript.cs
@@ -5,27 +5,29 @@ using UnityEngine;
 public class BordCellFactoryScript : MonoBehaviour
 {
     public GameObject bordSell;
+    
 
-    //ボードの縦横の大きさを指定
-    private const int CELL_HEIGHT_MAX = 8;
-    private const int CELL_SIDE_MAX = 8;
+    // ボードの縦横の大きさを指定
+    private const int BORD_HEIGHT_MAX = 8;
+    private const int BORD_SIDE_MAX = 8;
 
     // Start is called before the first frame update
     // 縦横それぞれ8マスで合計の64個のマスを作成
     void Start()
     {
-        int height = 0;
+        int z = 0;
 
-        while (height < CELL_HEIGHT_MAX)
+        while (z < BORD_HEIGHT_MAX)
         {
-            int side = 0;
+            int x = 0;
 
-            while (side < CELL_SIDE_MAX)
+            while (x < BORD_SIDE_MAX)
             {
-                Instantiate(bordSell, new Vector3(1.0f * side, 0f, 1.0f * height), transform.rotation);
-                side++;
+                bordSell.name = "bordSell(" + x + "," + z + ")";
+                Instantiate(bordSell, new Vector3(1.0f * x, 0f, 1.0f * z), transform.rotation);
+                x++;
             }
-            height++;
+            z++;
         }
     }
 
--- reversi_pjt/reversi/Assets/Scripts/BordScript.cs
+++ reversi_pjt/reversi/Assets/Scripts/BordScript.cs
@@ -10,15 +10,22 @@ public class BordScript : MonoBehaviour, IPointerClickHandler
     public Material[] materials;
 
     // ゲームマネージャー
-    GameObject gm;
-    GameManager gmScript;
+    private static GameObject gm;
+    private static GameManager gmScript;
+    private static Reversi reversi;
+
+    // ボードの石の描画状態を保持する配列
+    private static GameObject[,] pieceArray = new GameObject[8, 8];
 
     // Start is called before the first frame update
     void Start()
     {
         gm = GameObject.Find("GameManager");
         gmScript = gm.GetComponent<GameManager>();
+        reversi = new Reversi();
 
+        // ゲーム開始時にreversi.Boardの状態を確認し、ボードの描画状態を更新する。
+        ConfirmBord();
     }
 
     // Update is called once per frame
@@ -30,20 +37,68 @@ public class BordScript : MonoBehaviour, IPointerClickHandler
     // ボードクリック時の処理
     public void OnPointerClick(PointerEventData data)
     {
-        //石を表示
-        //piece.SetActive(true);
-        Instantiate(piece, transform.position, transform.rotation);
 
-        if (gmScript.player == "black")
+        // クリックされたマスから座標を取得する。
+        float x = transform.position.x;
+        float z = transform.position.z;
+        int x_int = (int)x;
+        int z_int = (int)z;
+        Debug.Log("座標(" + x_int + "," + z_int + ")");
+
+        // ReversiクラスのCanPutメソッドに座標を渡し、置くことができるか判定
+        bool putFlg = reversi.CanPut(x_int, z_int);
+        Debug.Log("置くことができるか判定" + putFlg);
+
+        // 置くことができたら、ReversiクラスのPutStoneメソッドに座標を渡し、情報を更新。
+        if (putFlg is true)
         {
-            //石を黒に変更
-            piece.GetComponent<Renderer>().material = materials[0];
-        }
-        else {
-            //石を白に変更
-            piece.GetComponent<Renderer>().material = materials[1];
-        }
+            reversi.PutStone(x_int, z_int);
 
+            // reversi.Boardの状態を確認し、ボードの描画状態を更新する。
+            ConfirmBord();
+        }
     }
 
+    // ボードの石の状態を確認するメソッド。石が置いてある場合描画を更新する。
+    private void ConfirmBord()
+    {
+        int mateBLACK = 0;
+        int mateWHITE = 1;
+
+        int[,] bord = reversi.Board;
+
+        int z = 0;
+        while (z < 8)
+        {
+            int x = 0;
+            while (x < 8)
+            {
+                int stoneColor = bord[x, z];
+                if (stoneColor == Reversi.BLACK)
+                {
+                    UpdatePieceArray(x, z, mateBLACK);
+                }
+                else if (stoneColor == Reversi.WHITE)
+                {
+                    UpdatePieceArray(x, z, mateWHITE);
+                }
+                x++;
+            }
+            z++;
+        }
+    }
+
+    // 石の描画を更新するメソッド。
+    private void UpdatePieceArray(int x, int z, int material)
+    {
+        if(pieceArray[x, z] is null)
+        {
+            pieceArray[x, z] = Instantiate(piece, new Vector3(x, 0, z), transform.rotation);
+            pieceArray[x, z].GetComponent<Renderer>().material = materials[material];
+        }
+        else
+        {
+            pieceArray[x, z].GetComponent<Renderer>().material = materials[material];
+        }
+    }
 }
